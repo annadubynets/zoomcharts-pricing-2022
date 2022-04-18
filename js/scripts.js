b0104cb411816be54b647441e45d5fb5bdfdc35c
Ivan Dubynets <ivan.dubynets@gmail.com>
2022-04-18 22:50:06 +0300

    added the inner page slider

--- js/scripts.js
+++ js/scripts.js
@@ -142,4 +142,79 @@ $(function() {
     initFormRangeControls('.form-range');
 });
 
-$('.img-area').hiSlide();
+
+$(function() {
+    if ($('.header-images-carousel').length) {
+        $('.header-images-carousel').hiSlide();
+    }
+});
+
+
+/**
+ * Inner's page slider
+ */
+
+$(function() {
+    var topSlider = $("#inner-slider-top");
+    var bottomSlider = $("#inner-slider-bottom");
+
+    if (!topSlider.length || !bottomSlider.length) {
+        return;
+    }
+
+    var thumbnailItemClass = '.owl-item';
+
+    var slides = topSlider.owlCarousel({
+        video:true,
+        startPosition: 1,
+        items:1,
+        loop:true,
+        margin:10,
+        autoplay: false,
+        nav: false,
+        dots: false
+    }).on('changed.owl.carousel', syncPosition);
+
+    function syncPosition(el) {
+        $owl_slider = $(this).data('owl.carousel');
+        
+        var current = el.item.index;
+        var realNumber = parseInt(topSlider.find(thumbnailItemClass).eq(current).find('.item').attr('data-number')) || 0;
+        
+        var thumbnailCurrentItem = bottomSlider.find(thumbnailItemClass).eq(realNumber);
+
+        if (!thumbnailCurrentItem.hasClass('centered')) {
+            var duration = 100;
+            bottomSlider.trigger('to.owl.carousel',[realNumber, duration, true]);
+        }   
+    }
+    var thumbs = bottomSlider.owlCarousel({
+        startPosition: 1,
+        items: 3,
+        loop: false,
+        margin: 3,
+        autoplay: false,
+        center: true,
+        nav: true,
+        dots: false,
+        navText : ['<i class="iconfont-arrow-prev"></i>','<i class="iconfont-arrow-next"></i>'],
+        onInitialized: function (e) {
+            var thumbnailCurrentItem =  $(e.target).find(thumbnailItemClass).eq(this._current);
+            console.log(thumbnailCurrentItem.find('.item').attr('data-number'))
+            setTimeout(function(){ 
+                $owl_slider = topSlider.data('owl.carousel');
+                $owl_slider.to(thumbnailCurrentItem.index(), 100, true);
+            }, 500)
+        },
+    })
+    .on('click', thumbnailItemClass, function(e) {
+        e.preventDefault();
+        $owl_slider = bottomSlider.data('owl.carousel');
+        $owl_slider.to($(this).index(), 100, true);
+    }).on("translated.owl.carousel", function (el) {
+        var centeredItem = $(el.target).find(".owl-item.active.center");
+        $owl_slider = topSlider.data('owl.carousel');
+        $owl_slider.to(centeredItem.index(), 100, true);
+    });
+
+});
